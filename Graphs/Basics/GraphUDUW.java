5e9d3ddfd7c8ef80f57dc31fc4af851c223ad24e
Sucheta Nandi <142592882+SuchetaNandi@users.noreply.github.com>
2025-05-28 22:32:25 +0530

    Add files via upload

---
+++ Graphs/Basics/GraphUDUW.java
@@ -0,0 +1,47 @@
+package Graphs;
+
+import java.util.*;
+
+public class GraphUDUW {
+    static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int s, int d){
+            this.src= s;
+            this.dest= d;
+        }
+    } 
+
+    // undirected unweighted graph
+    public static void createGraph(ArrayList<Edge> graph[]){
+        // we initialize elements of graph to an empty arraylist as it is null by default which might throw error
+        for(int i=0; i<4; i++){
+            graph[i]= new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0,2));
+
+        graph[1].add(new Edge(1,2));
+        graph[1].add(new Edge(1,3));
+
+        graph[2].add(new Edge(2,0));
+        graph[2].add(new Edge(2,1));
+        graph[2].add(new Edge(2,3));
+
+        graph[3].add(new Edge(3,1));
+        graph[3].add(new Edge(3,2));
+    }
+
+    public static void main(String args[]){
+        int V=4;
+        ArrayList<Edge> graph[]= new ArrayList[V];
+        createGraph(graph);
+
+        //printing the neighbours of 2
+        for(int i=0; i< graph[2].size(); i++){
+            Edge e= graph[2].get(i);
+            System.out.print(e.dest+" ");
+        }
+    }
+}
